046be68aee1f30c89663190613fdc91c120a4e91
kiomi <mehditouriki1@gmail.com>
2024-04-09 12:11:13 +0000

    fixed directories and templates done

---
+++ website/users/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.home),
+    path('login/', views.login),
+    path('recruteur/', views.home_rec),
+    path('candidat/', views.home_can),
+    path('recruteur/signuprec/', views.signuprec),
+    path('candidat/signupcan/', views.signupcan),
+    path('recruteur/formulaire', views.post_job),
+    # todo: modify this path for dynamic description mr.database
+    path('candidat/description', views.description)
+]
--- website/users/views.py
+++ website/users/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def home(request):
+    return render(request, "home.html")
+
+
+def signuprec(request):
+    return render(request, "recruteur/signuprec.html")
+
+
+def signupcan(request):
+    return render(request, "candidat/signupcan.html")
+def home_can(request):
+    return render(request, "candidat/jobs.html")
+def home_rec(request):
+    return render(request, "recruteur/index.html")
+def post_job(request):
+    return render(request, "recruteur/formulaire.html")
+def login(request):
+    return render(request, "login.html")
+def description(request):
+    # todo: fill the context parameter
+    return render(request, "candidat/description.html", context={})
