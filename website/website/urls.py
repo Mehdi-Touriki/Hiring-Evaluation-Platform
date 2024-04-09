c963c28bd62c071898b50a70f7859702ff7f6077
kiomi <mehditouriki1@gmail.com>
2024-04-09 11:00:12 +0000

    initialiazing django project

--- website/website/urls.py
+++ website/website/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for cv_analysis_website project.
+URL configuration for website project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
