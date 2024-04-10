9a2c7f24f2998b4444869e2ebd442d2ef4815dc3
kiomi <mehditouriki1@gmail.com>
2024-04-10 10:07:19 +0000

    New Class based views for joblist and job details

--- website/jobs/views.py
+++ website/jobs/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
 
-# Create your views here.
+from django.views.generic import (
+    ListView,
+    DetailView,
+    DeleteView,
+    UpdateView,
+    CreateView
+)
+from .models import JobOffer
+
+
+def apply(request):
+    return render(request, "")
+
+
+class JobListView(LoginRequiredMixin, ListView):
+    model = JobOffer
+    template_name = "jobs/jobs.html"  # <app>/<model>_<viewtype>.html
+    context_object_name = "jobs"
+    ordering = ["-date_posted"]
+
+
+class JobDescriptionView(LoginRequiredMixin, ListView):
+    model = JobOffer
+    template_name = "jobs/description.html"
+    context_object_name = "job"
+
+
+class JobApplyView:
+    pass
+
+
+class JobDeleteView(DeleteView):
+    pass
+
+
+class JobCreateView(CreateView):
+    pass
+
+
+class JobUpdateView(UpdateView):
+    pass
