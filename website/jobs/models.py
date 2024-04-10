be75d94629b1b372cbac3d54ef6b8ea9a5eef971
Ilyass Toumi <toumi.ilyass.04@gmail.com>
2024-04-10 09:22:34 +0000

    database

--- website/jobs/models.py
+++ website/jobs/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.utils import timezone
 # Create your models here.
+from django.db import models
+
+class Post(models.Model):
+    job_title = models.CharField(max_length=100)
+    job_type = models.CharField(max_length=100)
+    job_location = models.CharField(max_length=100)  
+    description = models.TextField()
+    salary = models.IntegerField(null=True, blank=True)
+    publication_data = models.DateField(default=timezone.now) 
+    requirements = models.TextField()  
+    recruiter = models.ForeignKey(User, on_delete=models.CASCADE)  
+
+    def __str__(self):
+        return self.job_title
+
+
