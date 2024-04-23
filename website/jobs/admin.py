73758424029e26629a512966e86fe074eb1f56bd
wadia20 <wadiasouiki@gmail.com>
2024-04-23 19:37:01 +0100

    wadia lli dar koulchi

--- website/jobs/admin.py
+++ website/jobs/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Post, ApplyJob
+from .models import Post, ApplyJob,Profil,SavedPostt
 
 admin.site.register(Post)
 admin.site.register(ApplyJob)
+admin.site.register(Profil)
+admin.site.register(SavedPostt) # hadi tzadt
--- website/jobs/urls.py
+++ website/jobs/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
-from . import views
+from . import views 
 from .views import(
     JobListView,
+    saved_post,
     JobDescriptionView,
     JobDeleteView,
     JobUpdateView,
     AllApplicants,
-    MyJobListView)
+    MyJobListView,
+    MysaveJobListView)
 
 app_name = 'jobs'
 
@@ -14,7 +16,11 @@ urlpatterns = [
     path('jobs/', JobListView.as_view(), name='job_list'),
     path('jobs/<int:pk>/', JobDescriptionView.as_view(), name='job_description'),
     path('jobs/<int:pk>/apply/', views.job_apply_view, name='job_apply'),
+    path('jobs/profil/', views.profil, name='profil'),  # Updated URL pattern here
+    path('jobs/saved/', saved_post.as_view(), name='saved'),# Updated URL pattern here hta hada
+    
     path('recruteur/new/', views.job_create_view, name='job_create'),
+    path('jobs/my_saves/', MysaveJobListView.as_view(), name='my_saves'),#o hada
     path('recruteur/myjobs/', MyJobListView.as_view(), name='my_jobs'),
     path('recruteur/', views.post_job, name='post_job'),
     path('recruteur/myjobs/<int:pk>/update/', JobUpdateView.as_view(), name='job_update'),
