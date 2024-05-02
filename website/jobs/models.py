7c3dec664204d95d08f770c1a20c8ab9b3b57c62
chaimae-zemri <c.zemri.26@gmail.com>
2024-05-02 14:16:27 +0200

    list of possible categories

--- website/jobs/models.py
+++ website/jobs/models.py
@@ -5,12 +5,16 @@ from django.urls import reverse
 from datetime import datetime
 
 JOB_CATEGORIES = [
-    ('category1', 'Category 1'),
-    ('category2', 'Category 2'),
-    ('category3', 'Category 3'),
-    ('category4', 'Category 4'),
-    ('category5', 'Category 5'),
-    ('category6', 'Category 6'),
+    ('category1', 'Informatique Et Technologie '),
+    ('category2', 'Finance et Comptabilité'),
+    ('category3', 'Ressources Humaines'),
+    ('category4', 'Santé Et Médical'),
+    ('category5', 'Construction Et Travaux Publics'),
+    ('category6', 'Hôtellerie et Restauration'),
+    ('category7', 'Logistique Et Transport'),
+    ('category8', 'Media Et Communication'),
+    ('category9', 'Art Et Design'),
+    ('category10', 'Ingénierie'),
 ]
 class Post(models.Model):
     job_category=models.CharField(max_length=50, choices=JOB_CATEGORIES)
